2c05d9e9bd5b22c2730936d84d7515052d78177d
Emesye <ayupratitiz@gmail.com>
2023-03-06 19:58:34 +0700

    hihihi

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+
+@section ('container')
+<h1>Selamat datang blog ini merupakan blog makanan.</h1>
+        <p>Memuat makanan berpa donat dan sambal pecel.</p>
+@endsection
+   
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Frontend\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,56 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::resource('/home', HomeController::class);
+Route::resource('/admin', DashboardController::class);
+
+Route::get('masayu', function () {
+    return "haloo";
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+Route::get('/about', function(){
+    return view('about',[
+        "judul" => "About",
+        "nama" => "Masayu Pratitis",
+        "Nim" => "E41210489",
+        "image" => "yoona.jpg"
+    ]);
+});
+Route::get('/dashboard', function(){
+    return view('dashboard',[
+        "judul" => "Dashboard"
+    ]);
+});
+Route::get('/blog', function(){
+    $blog_post=[
+        [
+            "judul" => "Blog Pertama",
+            "Penulis" => "Masayu Pratitis",
+            "body" =>"Donat adalah panganan yang digoreng dibuat dari tepung terigu, gula pasir, ragi, roti dan mentega. dan umumnya donat berbentuk seperti cincin dengan 
+            lubang ditengah, namun kini seiring berkembangnya jaman donat sekarang tersedia dengan berbagai macam toping danventuk yang lucu"
+        ],
+        [
+            "judul" => "Blog Kedua",
+            "Penulis" => "Adisti yudia",
+            "body" =>"Pecel merupakan panganan khas madiun yang umumnya disajikan dengan nasi panas dengan kulupan atau sayuran rebus serta bumbu kacang, 
+            sambal pecel sendiri terbuat dari kacang yang digiling halus,cabai,asam jawa,daun jeruk dan berbagai macam jenis bahan lainnya."
+        ]
+        ];
+    return view('blog',[
+        "judul" => "Blog",
+        "postingan"=> $blog_post
+    ]);
+
+});
+
+
+Route::get('/admin', function(){
+    return view('admin',[
+        "judul" => "Dashboard"
+    ]);
 });
